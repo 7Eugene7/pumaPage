7e25f2c6ba7b42bf9a607fde47e878d938b15e52
7Eugene7 <90318813+7Eugene7@users.noreply.github.com>
2022-11-28 10:18:41 +0200

    done tabs

--- js/style.js
+++ js/style.js
@@ -58,4 +58,23 @@ if (isMobile.any()) {
     body.classList.add('mouse');
 }
 
+// Tabs for gallery
+const tabs = document.querySelector(".gallery__container");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
 
+tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if (id) {
+        tabButton.forEach(btn => {
+            btn.classList.remove("active");
+        });
+        e.target.classList.add("active");
+
+        contents.forEach(content => {
+            content.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+}
